a8269bf79c6deb491033350c1153a8774cf64ed0
michaelgund <>
2021-05-26 11:23:45 +0000

    Initial commit

---
+++ variables.py
@@ -0,0 +1,8 @@
+age = 30                # variable assigned a value
+
+print(age)              # print the age which has value 30
+
+friend_age = 23          # we use underscore for variables
+countries_visited = 90    # with long names
+
+PI = 3.14159             # We use CAPS for variables with constant values
