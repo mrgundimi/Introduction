229463220922fc4e3475571c9dc05c22c016a495
michaelgund <>
2021-05-26 11:37:53 +0000

    numbers

---
+++ numbers.py
@@ -0,0 +1,11 @@
+age = 35              #integer
+
+PI = 3.14             # float
+
+math_operation = 1 + 3 * 4 / 2 - 2
+
+print(math_operation)
+
+math_operation_as_integer = 1 + 3 * 4 // 2 - 2
+
+print(math_operation_as_integer)
